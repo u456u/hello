c9c34f000afbd7854775730e4f015c1a02b091d5
u456u <898619304@qq.com>
2021-06-11 15:31:26 +0800

    李根第一次提交

---
+++ test_jk/src/test/java/com/lg/test_jk/TestJkApplicationTests.java
@@ -0,0 +1,14 @@
+package com.lg.test_jk;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestJkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
